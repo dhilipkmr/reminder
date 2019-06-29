0d30bb5326bd0d7f5bf2c150f22771474ce9f55f
dhilipkmr <dhilip1211@gmail.com>
2019-06-29 18:29:08 +0530

    Testcase

--- test/test.js
+++ test/test.js
@@ -7,8 +7,8 @@ chai.use(chaiHttp);
 const Promise = require("bluebird");
 const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
-const {Builder, By, Key, until} = require('selenium-webdriver'),
-    chrome = require('selenium-webdriver/chrome');
+var chaiWebdriver = require('chai-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver'), chrome = require('selenium-webdriver/chrome');
 var driver;
 
 const options = new chrome.Options();
@@ -24,7 +24,7 @@ var rgbToHex = function (rgb) {
   return hex;
 };
 
-describe('list view test', function() {
+describe('Reminder Test Results: \n', function() {
     this.timeout(100000);
 
     before(function(done){
@@ -32,9 +32,10 @@ describe('list view test', function() {
             .forBrowser('chrome')
             .setChromeOptions(options)
             .build();
+        chai.use(chaiWebdriver(driver));
         driver.get('http://localhost:8000')
             .then(()=>{
-                console.log("Got page");
+               
                 done();
             });
     });
@@ -47,65 +48,75 @@ describe('list view test', function() {
         let page = await driver.getPageSource();
     });
 
-    it('should ignore empty input and not add new element to list', async function(){
-        let input = await driver.findElement(By.id("input"));
-        input.sendKeys("");
-        let val = await input.getAttribute('value');
-
-           let button = await driver.findElement(By.id("button"));
-           let bevent = await button.click();
-
-           let list = await driver.findElements(By.tagName("li"));
-           expect(list.length).to.equal(0);
-
-           input.sendKeys(" ");
-        val = await input.getAttribute('value');
-
-           button = await driver.findElement(By.id("button"));
-           bevent = await button.click();
-
-           list = await driver.findElements(By.tagName("li"));
-           expect(list.length).to.equal(0);
+    it ('should Render Page with Fields \'timerName\',\'time\' and \'addBtn\'', async function() {
+        let inputField1 = await driver.findElement(By.id('timerName'));
+        let inputField2 = await driver.findElement(By.id('time'));
+        let addbButton = await driver.findElement(By.id('addBtn'));
+        expect(inputField1).dom.to.be.visible();
+        expect(inputField2).dom.to.be.visible();
+        expect(addbButton).dom.to.be.visible();
 
     });
+    
+    // it('should ignore empty input and not add new element to list', async function(){
+    //     let input = await driver.findElement(By.id("input"));
+    //     input.sendKeys("");
+    //     let val = await input.getAttribute('value');
 
-    it('should get input and add new element to list', async function(){
-            let input = await driver.findElement(By.id("input"));
-            input.sendKeys("USA");
-            let val = await input.getAttribute('value');
+    //        let button = await driver.findElement(By.id("button"));
+    //        let bevent = await button.click();
 
-               let button = await driver.findElement(By.id("button"));
-               button.click();
+    //        let list = await driver.findElements(By.tagName("li"));
+    //        expect(list.length).to.equal(0);
 
-               let list = await driver.findElements(By.tagName("li"));
-               expect(list.length).to.equal(1);
+    //        input.sendKeys(" ");
+    //     val = await input.getAttribute('value');
 
-    });
+    //        button = await driver.findElement(By.id("button"));
+    //        bevent = await button.click();
 
-    it('should check for red color of text for each 3rd element', async function(){
+    //        list = await driver.findElements(By.tagName("li"));
+    //        expect(list.length).to.equal(0);
+
+    // });
+
+    // it('should get input and add new element to list', async function(){
+    //         let input = await driver.findElement(By.id("input"));
+    //         input.sendKeys("USA");
+    //         let val = await input.getAttribute('value');
+
+    //            let button = await driver.findElement(By.id("button"));
+    //            button.click();
+
+    //            let list = await driver.findElements(By.tagName("li"));
+    //            expect(list.length).to.equal(1);
+
+    // });
+
+    // it('should check for red color of text for each 3rd element', async function(){
         
-            let input = await driver.findElement(By.id("input"));
-            let button = await driver.findElement(By.id("button"));
-            input.clear();
-            input.sendKeys("India");
-               button.click();
-               input.clear();
-            input.sendKeys("France");
-               button.click();
-               input.clear();
-            input.sendKeys("Spain");
-               button.click();
-               input.clear();
+    //         let input = await driver.findElement(By.id("input"));
+    //         let button = await driver.findElement(By.id("button"));
+    //         input.clear();
+    //         input.sendKeys("India");
+    //            button.click();
+    //            input.clear();
+    //         input.sendKeys("France");
+    //            button.click();
+    //            input.clear();
+    //         input.sendKeys("Spain");
+    //            button.click();
+    //            input.clear();
 
-               let list = await driver.findElements(By.tagName("li"));
-               let value = await list[2].getCssValue('color');
-               value = value.substring(5, value.length-1)
-             .replace(/ /g, '')
-             .split(',');
+    //            let list = await driver.findElements(By.tagName("li"));
+    //            let value = await list[2].getCssValue('color');
+    //            value = value.substring(5, value.length-1)
+    //          .replace(/ /g, '')
+    //          .split(',');
 
-            value.pop();
-            let checkValue = value.reduce( (prev, curr) => prev + rgbToHex(curr), "#");
-               expect(checkValue).to.equal('#ff0000');
+    //         value.pop();
+    //         let checkValue = value.reduce( (prev, curr) => prev + rgbToHex(curr), "#");
+    //            expect(checkValue).to.equal('#ff0000');
 
-    });
+    // });
 });
